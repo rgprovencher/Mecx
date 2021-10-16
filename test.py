faceae77cd222cd97be4cf3808c1a80ba409a4a6
rgprovencher <57636241+rgprovencher@users.noreply.github.com>
2021-10-16 17:27:42 +0000

    updated test with a pin checker

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-#!/usr/bin/python373all
-import cgi
+#! /usr/bin/python373all
+
+import RPi.GPIO as GPIO
+
+pin = 14
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN)
 
 print("Content-type: text/html\n\n")
-data = cgi.FieldStorage()
-print("selection = " + data.getvalue('two_buttons'))
+print('<html><body><font color = red'>')
+
+
+print('GPIO pin % is: ' %pin, end = '')
+if GPIO.input(pin):
+  print("HIGH")
+else:
+  print("LOW")
+
+print('</body></html>')
