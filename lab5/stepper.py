8c4b0d48cd14027977a8d6e184dc127785879bcd
rgprovencher <57636241+rgprovencher@users.noreply.github.com>
2021-10-27 20:09:34 +0000

    added zero and moveto functions

--- lab5/stepper.py
+++ lab5/stepper.py
@@ -2,6 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
+FULL_ANGLE = 550      # no. of half to move the cardboard arm by one cardboard width
+HALF_ANGLE  = 275     # no. of halfsteps to turn the arm by one half cardboard width
+
 class Stepper:
 
     def __init__(self, p0, p1, p2, p3, led, pcf):
@@ -18,9 +21,13 @@ class Stepper:
         self.sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
                 [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
         
-        self.state = 0
+        self.state = 0 # tracks location within the stator sequence
         
-        self.theta = 180
+
+        # defines the current angle of the cardboard arm, with the LED at 180.
+        # initialized to an arbitrary value, then calibrated with an initial zero() call to start at 180 (covering the LED).
+        self.theta = 90
+        self.zero()
        
 
     def delay_us(self, tus): # use microseconds to improve time resolution
@@ -54,15 +61,6 @@ class Stepper:
         for step in range(steps):
             self.halfstep(dir)
     
-    def zero(self, dir):
-        GPIO.output(self.pins[4], 1)       # turn on LED
-        calibration = self.pcf.read(0)     # take a reference reading
-        
-        
-        
-        GPIO.output(self.pins[4], 0)       # turn off LED
-    
-    
     
     # reports current angle when called. The class considers the position of
     # the LED to be 180 degrees, which simplifies some logic in other
@@ -71,7 +69,62 @@ class Stepper:
     # be at zero.
     def angle(self):
         return (theta+180)%360
+
         
-        
+    # given an angle, determines the nearest direction for the arm to
+    # turn. Returns +1 for ccw, -1 for cw.
     def nearest(self, angle):
+      
+      # class considers LED to be at 180. If current angle < 180, return
+      # 1 to turn arm cw, otherwise -1 to turn arm ccw.
+
+      if 180 - dir > 0: dir = 1
+      else: dir = -1
+
+      return dir
+
+    def goAngle(self, angle):
+      # short circuits any clever clogs who want to type in out of range angles.
+      # adds 180 to input angle bc to user, LED is at 0; to class LED is at 180.
+      angle = (angle+180) % 360 
+
+      # find nearest turning direction and number of degrees to turn
+      dir = nearest(angle) 
+      angle = abs(float(angle-self.theta))
+
+      # # 8 half-steps per cycle, 8 cycles per revolution, into a 1:64 gearbox = half-steps per full revolusion 
+      # stepAngle = 8*8*64
+      # # 360 degrees per rev  /  no. of half-steps pre rev = degrees per half-step
+      # stepAngle = float(360)/float(stepAngle)
+      # = 0.0879 deg; no need to calculate that every time.
+      stepAngle = 0.0879
+
+      # calculate number of half-steps to turn
+      noSteps = int(angle/stepAngle)
+
+      # move that many steps in the nearest direction.
+      moveSteps(noSteps, dir)
+
+      
+
+
+    def zero(self, dir):
+        GPIO.output(self.pins[4], 1)       # turn on LED
+        calibration = self.pcf.read(0)     # take a reference reading
+        dir = nearest(0)                   # find nearest direction to 0
+        
+        # turn until photocell registers a change 
+        while (abs(calibration-self.pcf.read(0)) < 5):
+          halfstep(dir)
+
+        # if the value of the photocell DECREASES, it means the arm was covering the photocell when this command was entered, and moving has just uncovered it; the direction needs to be reversed.
+        if self.pcf.read(0) < calibration: dir += -1
+        
+        # rotates the cardboard one half-cardboard width to center it in front of the LED
+        movesteps(HALF_ANGLE, dir)
+
+        # resets internal angle.
+        self.theta = 180
+
+        GPIO.output(self.pins[4], 0)       # turn off LED
         
